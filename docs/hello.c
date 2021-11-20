e332fccfd5d6421609b33bd29b32515a6eb6b7e2
Alexander Tuzhik <alprog@hotmail.com>
2021-11-20 01:42:34 +0100

    emscripten

---
+++ docs/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <iostream>
+
+int main() {	
+  std::cout << "hello, world!\n";
+  
+  char a;
+  std::cin >> a;
+  std::cout << a << std::endl;
+  
+  std::cin >> a;
+  std::cout << a << std::endl;
+  
+  
+  return 0;
+}
