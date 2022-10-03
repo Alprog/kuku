99bbb86cc6f6ef0ec8534f47c6a089f5f5278c20
Alexander Tuzhik <alprog@hotmail.com>
2022-10-03 19:33:55 +0200

    remove grammatool

--- tools/grammartool/Production.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Symbol.h"
-#include <vector>
-
-struct Production
-{
-    std::vector<Symbol*> Symbols;
-};
--- tools/grammartool/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> split(std::string line, std::string delimeter);
